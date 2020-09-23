3e5cdc86ad871ef3f3873a3810bd62803f8bf29c
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2020-09-23 22:33:30 +0400

    feat: add sqrt and extend

--- calculator/script.js
+++ calculator/script.js
@@ -1,106 +1,129 @@
-const operandArrayEl = document.getElementById('operandArray');
-const currentOperandEl = document.getElementById('currentOperand');
+const operandArrayEl = document.getElementById("operandArray");
+const currentOperandEl = document.getElementById("currentOperand");
 
-currentOperandEl.innerHTML = '0';
+currentOperandEl.innerHTML = "0";
 
 let operandArray = [];
-let currentOperand = '0';
+let currentOperand = "0";
 
 const del = () => {
   currentOperand = currentOperand.slice(0, -1);
   updateView();
-}
+};
 
 const clearAll = () => {
-  operandArrayEl.innerHTML = '';
-  currentOperandEl.innerHTML = '0';
-  currentOperand = '0';
+  operandArrayEl.innerHTML = "";
+  currentOperandEl.innerHTML = "0";
+  currentOperand = "0";
   operandArray = [];
-}
+};
 
 const addOperand = (operand) => {
+  // if (operand === '-' && operandArray.length > 0) {
+  //   currentOperand *= -1 ;
+  // } else {
+  //   currentOperand = '0';
+  // }
   operandArray.push(currentOperand);
-  currentOperand = '0';
+  currentOperand = "0";
   operandArray.push(operand);
   updateView();
-}
+};
 
 const addNumber = (number) => {
-  if (currentOperand === '0' || currentOperand === 0) {
+  if (currentOperand === "0" || currentOperand === 0) {
     currentOperand = number.toString();
   } else {
     currentOperand += number.toString();
   }
   updateView();
-}
+};
 
 const addDot = () => {
-  if (currentOperand.indexOf('.') < 0) {
-    currentOperand += '.';
+  if (currentOperand.indexOf(".") < 0) {
+    currentOperand += ".";
   }
   updateView();
-}
+};
 
 const checkResult = () => {
   operandArray.push(currentOperand);
   let queue = createQueue(operandArray);
   let copy = [...operandArray];
-  recursiveCalc(0, copy, queue)
+  recursiveCalc(0, copy, queue);
   currentOperand = copy[0];
   setResult();
-}
+};
 
 const recursiveCalc = (acc = 0, array, queue) => {
   if (queue) {
     let localArr = [];
     let step = queue[0];
-      let operand = array[step];
-      let result = 0;
-      switch (operand) {
-        case '*':
-          result = +array[step-1] * +array[step+1];
-          break;
-        case '/':
-          result = +array[step-1] / +array[step+1];
-          break;
-        case '+':
-          result = +array[step-1] + +array[step+1];
-          break;
-        case '-':
-          result = +array[step-1] - +array[step+1];
-          break;
-      }
-      array[step+1] = result;
-      array.splice(step-1, 2);
-      localArr = array;
+    let operand = array[step];
+    let result = 0;
+    let firstOperand = +array[step - 1];
+    let secondOperand = +array[step + 1];
+    switch (operand) {
+      case "*":
+        result = firstOperand * secondOperand;
+        break;
+      case "/":
+        result = firstOperand / secondOperand;
+        break;
+      case "+":
+        result = firstOperand + secondOperand;
+        break;
+      case "-":
+        result = firstOperand - secondOperand;
+        break;
+      case "√":
+        if (secondOperand >= 0) {  
+          result = secondOperand ** (1 /firstOperand);
+        } else {
+          result = "err";
+        }
+        break;
+      case "^":
+        result = Math.pow(firstOperand, secondOperand);
+        break;
+    }
+    if (result === 'err') {
+      clearAll();
+      currentOperandEl.innerHTML = 'Error: negative sqrt number';
+      return;
+    }
+    result = Math.floor(result * 1000) / 1000 ;
+    array[step + 1] = result;
+    array.splice(step - 1, 2);
+    localArr = array;
     recursiveCalc(acc, localArr, createQueue(localArr));
   } else {
     return;
   }
-}
+};
 
-const createQueue = (array, operandArray = ['^', '√', '*', '/', '+', '-']) => {
+const createQueue = (array, operandArray = ["^", "√", "*", "/", "+", "-"]) => {
   const queue = [];
   operandArray.forEach((operand) => {
     array.forEach((value, index) => {
       if (operand === value) {
         queue.push(index);
       }
-    })
-  })
+    });
+  });
   if (queue.length < 1) {
     return null;
   }
   return queue;
-}
+};
 
 const updateView = () => {
   currentOperandEl.innerHTML = currentOperand;
-  operandArrayEl.innerHTML = operandArray.join('');
-}
+  operandArrayEl.innerHTML = operandArray.join("");
+};
 
 const setResult = () => {
-  operandArrayEl.innerHTML = '';
+  operandArrayEl.innerHTML = "";
   currentOperandEl.innerHTML = currentOperand;
   operandArray = [];
-}
+};
