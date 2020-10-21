0a52e5310a2c493f805aa658e294412060fcdc81
YaroslavTrefilov <yaroslavtrefilov5@gmail.com>
2020-10-21 20:06:12 +0400

    feat: add weather api

--- momentum/script.js
+++ momentum/script.js
@@ -255,10 +255,44 @@ const quote = () => {
   document.addEventListener('DOMContentLoaded', getQuote);
   btn.addEventListener('click', getQuote);
 };
+
+// 
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
+
+const getWeatherForCity = () => {
+  const weatherIcon = document.querySelector('.weather-icon');
+  const temperature = document.querySelector('.temperature');
+  const weatherDescription = document.querySelector('.weather-description');
+  const city = document.querySelector('.city');
+  
+  async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+    
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+  }
+  
+  function setCity(event) {
+    if (event.code === 'Enter') {
+      getWeather();
+      city.blur();
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', getWeather);
+  city.addEventListener('keypress', setCity);
+};
+getWeatherForCity();
+// 
+
 quote();
-
-
-
 getFocus();
 getName();
 changeBg();
