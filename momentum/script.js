9acc33171761cc8c170e1d173c6c0ba71580fe94
YaroslavTrefilov <yaroslavtrefilov5@gmail.com>
2020-10-21 17:07:28 +0400

    feat: add quote

--- momentum/script.js
+++ momentum/script.js
@@ -240,6 +240,24 @@ focus.addEventListener('click', clearFocus);
 focus.addEventListener('change', setFocus);
 focus.addEventListener('keypress', setFocus)
 
+const quote = () => {
+  const blockquote = document.querySelector('blockquote');
+  const figcaption = document.querySelector('figcaption');
+  const btn = document.querySelector('.btn-quote');
+
+  async function getQuote() {  
+    const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en`;
+    const res = await fetch(url);
+    const data = await res.json(); 
+    blockquote.textContent = data.quoteText;
+    figcaption.textContent = data.quoteAuthor;
+  }
+  document.addEventListener('DOMContentLoaded', getQuote);
+  btn.addEventListener('click', getQuote);
+};
+quote();
+
+
 
 getFocus();
 getName();
